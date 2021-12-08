7f3018b764c4dd020fc3d61181a9ecbec8b19ad2
Mariana Costa <mariana.costa214@gmail.com>
2021-12-08 20:01:44 +0100

    Keep constant num of rocks spawned

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -7,6 +7,13 @@ var screen_size
 
 var rng = RandomNumberGenerator.new()
 
+func spawn_asteroid():
+	var asteroid_instance = asteroid_scene.instance()
+	self.add_child(asteroid_instance)
+	asteroid_instance.global_position.x = rng.randf_range(0, screen_size.x)
+	asteroid_instance.global_position.y = rng.randf_range(0, screen_size.y)
+	asteroids.append(asteroid_instance)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -14,15 +21,11 @@ func _ready():
 	rng.randomize()
 	var asteroid_instance
 	for n in range(10):
-		asteroid_instance = asteroid_scene.instance()
-		add_child(asteroid_instance)
-		
-		asteroid_instance.global_position.x = rng.randf_range(0, screen_size.x)
-		asteroid_instance.global_position.y = rng.randf_range(0, screen_size.y)
-		
-		asteroids.push_back(asteroid_instance)
+		spawn_asteroid()
 	
 
-
 func _process(delta):
-	pass
+	for i in range(0, asteroids.size()):
+		if !is_instance_valid(asteroids[i]):
+			asteroids.remove(i)
+			spawn_asteroid()
