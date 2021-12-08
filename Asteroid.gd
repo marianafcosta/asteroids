5ce408c666cc0e15882344ab8742a3ea7cd17f48
Mariana Costa <mariana.costa214@gmail.com>
2021-12-08 09:03:09 +0100

    Add movement to rocks

--- Asteroid.gd
+++ Asteroid.gd
@@ -1,16 +1,26 @@
 extends Area2D
 
+var speed = 50
+var direction
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var screen_size
 
+var rng = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
+func out_of_bounds(x, y):
+	var sprite_half_width = self.get_node("Sprite").texture.get_width() / 2
+	var sprite_half_height = self.get_node("Sprite").texture.get_height() / 2
+	return (x > screen_size.x + sprite_half_width || x < -sprite_half_width || y > screen_size.y + sprite_half_height || y < -sprite_half_height)
+
 func _ready():
-	pass # Replace with function body.
+	rng.randomize()
+	direction = Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1)).normalized()
+	screen_size = get_viewport_rect().size
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = direction * speed
+	position += velocity * delta
+	
+	if (out_of_bounds(position.x, position.y)):
+		# TODO: Check if asteroid references are being removed from the array in the EnemySpawner
+		self.queue_free()
