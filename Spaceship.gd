38f02371c95340fa7da66444444620e3477c59c0
Mariana Costa <mariana.costa214@gmail.com>
2021-12-11 11:24:06 +0100

    Add enemy spaceship attack

--- Spaceship.gd
+++ Spaceship.gd
@@ -6,7 +6,16 @@ var life = 3
 
 var screen_size
 
+var projectile_scene = preload("res://EnemyProjectile.tscn")
+
 var rng = RandomNumberGenerator.new()
+
+func shoot():
+	var projectile_instance = projectile_scene.instance()
+	projectile_instance.init(direction)
+	get_tree().get_root().add_child(projectile_instance)
+	projectile_instance.global_position.x = position.x
+	projectile_instance.global_position.y = position.y
 
 func out_of_bounds(x, y):
 	var sprite_half_width = self.get_node("Sprite").texture.get_width() / 2
@@ -16,7 +25,9 @@ func out_of_bounds(x, y):
 func _ready():
 	rng.randomize()
 	direction = Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1)).normalized()
+	self.rotate(Vector2(0,1).angle_to(direction))
 	screen_size = get_viewport_rect().size
+	$Timer.start()
 
 func _process(delta):
 	var velocity = direction * speed
@@ -30,3 +41,9 @@ func _on_Spaceship_area_entered(area):
 	life -= 1
 	if (life <= 0):
 		self.queue_free()
+
+func _on_Timer_timeout():
+	print("hi")
+	$Timer.start()
+	shoot()
+	pass # Replace with function body.
